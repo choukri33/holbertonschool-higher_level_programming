9123783ded1be5aa75fafffef9d3c714ca7a058e
choukri33 <7646@holbertonstudents.com>
2024-02-16 16:59:19 +0000

    commentary 1-my_list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+"""create class Mylist"""
+
+
 class MyList(list):
+    """class from list"""
     def print_sorted(self):
+        """print sorted list"""
         print(sorted(self))
