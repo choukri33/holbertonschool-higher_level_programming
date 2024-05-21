877bfb32111c13aceea7299fed6a793a4f62dc1b
choukri33 <7646@holbertonstudentss.com>
2024-05-21 13:53:59 +0000

    add 3-safe

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def safe_print_modulo(a, b):
+def safe_print_division(a, b):
     try:
         div = a / b
     except (TypeError, ZeroDivisionError):
         div = None
     finally:
-        print("Inside result: {}".format(mod))
-    return div
+        print("Inside result: {}".format(div))
+    return (div)
