fe294b57aaea26c44935d875db84af58201e7e7c
choukri33 <7646@holbertonsqtudents.com>
2024-05-23 09:37:17 +0000

    add 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
+
 def lookup(obj):
 	return dir(obj)
-=======
-"""Define Function lookup"""
 
->>>>>>> 9123783ded1be5aa75fafffef9d3c714ca7a058e
+"""Define Function lookup"""
 
 def lookup(obj):
     """Returns list object"""
--- python-inheritance/1-my_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""class MyList that inherits from list"""
-
-
-class MyList(list):
-    """class from list"""
-    def print_sorted(self):
-        """print sorted list"""
-        print(sorted(self))
