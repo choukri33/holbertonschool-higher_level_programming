c28e96cca04e622c092cf4cdd8134be0e883f4ae
choukri33 <7646@holbertonstudents.com>
2024-05-30 11:10:15 +0000

    test

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,4 +5,4 @@ def write_file(filename="", text=""):
     with open(filename, 'w', encoding='utf-8') as f:
         """write sting and count character"""
         chars_written = f.write(text)
-        return chars_written
+        return len(text)
