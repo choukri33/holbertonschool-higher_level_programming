a0545de443a74200a91b2b0ad4f7b5262a89ef56
choukri33 <7646@holbertonstudents.com>
2024-05-30 12:19:50 +0000

    add 1-write

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,6 +4,16 @@
 
 
 def write_file(filename="", text=""):
+    """
+    Writes the input text to a file specified by the filename.
+    
+    Args:
+        filename (str): The name of the file to write to.
+        text (str): The text to write to the file.
+        
+    Returns:
+        int: The number of characters written to the file.
+    """
     with open(filename, 'w', encoding='utf-8') as f:
         """Write string and count characters"""
 
