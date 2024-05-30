2c0e5704f867c08868807ce02fe725b66f251e09
choukri33 <7646@holbertonstudents.com>
2024-05-30 11:04:19 +0000

    add len in 1-write

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,5 +2,6 @@ def write_file(filename="", text=""):
     """function that writes a string and returns the number"""
 
     with open(filename, 'w', encoding='utf-8') as f:
+        """write sting and count character"""
         chars_written = f.write(text)
-        return chars_written
+        return len(text)
