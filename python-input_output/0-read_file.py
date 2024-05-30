ff3ec392b8307203b858c6f108ff34db2a6614f5
choukri33 <7646@holbertonstudents.com>
2024-05-30 10:42:15 +0000

    pycodestyle task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Module containing a function to read and print a file"""
 
+
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints its content to stdout"""
     with open(filename, 'r', encoding='utf-8') as f:
         content = f.read()
         print(content, end="")
-
