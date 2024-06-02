1114b8ca4b51f4b98ec2f8d22f163c46e458fa80
choukri33 <7646@holbertonstudents.com>
2024-06-02 17:28:26 +0000

    add task 8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Function to get the dictionary representation of an object's attributes for JSON serialization"""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structures
+    (list, dictionary, string, integer, and boolean) for JSON serialization of an object.
+
+    Args:
+        obj: The object to serialize.
+
+    Returns:
+        dict: The dictionary representation of the object's attributes.
+    """
+    return obj.__dict__
