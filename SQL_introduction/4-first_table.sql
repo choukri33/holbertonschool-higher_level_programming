b970741c704b7869d8b2c1d8d4af8b77ab2f21b1
choukri33 <7646@holbertonstudents.com>
2024-06-20 09:03:25 +0000

    add commentaire

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
--- This script lists all tables in the specified database passed as an argument
+-- This script creates a table named first_table with columns id and name, if it does not already exist
 $CREATE TABLE IF NOT EXIST first_table (
     ID INT,
     NAME VARCHAR(256)
