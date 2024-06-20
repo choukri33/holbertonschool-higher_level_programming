b7c3d614c98b76f815c94f529ce826103427a708
choukri33 <7646@holbertonstudents.com>
2024-06-20 09:06:10 +0000

    add 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,21 +1,2 @@
--- Print the description of the table first_table
-SELECT 
-    CONCAT(
-        'CREATE TABLE `', 
-        TABLE_NAME, 
-        '` (\n', 
-        GROUP_CONCAT(
-            '  `', COLUMN_NAME, '` ', COLUMN_TYPE, 
-            IF(IS_NULLABLE = 'NO', ' NOT NULL', ''), 
-            IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', COLUMN_DEFAULT), ''), 
-            IF(EXTRA = '', '', CONCAT(' ', EXTRA))
-            ORDER BY ORDINAL_POSITION SEPARATOR ',\n'
-        ), 
-        '\n) ENGINE=', ENGINE, 
-        ' DEFAULT CHARSET=', TABLE_COLLATION
-    ) AS create_table_statement
-FROM information_schema.COLUMNS
-JOIN information_schema.TABLES 
-ON TABLES.TABLE_NAME = COLUMNS.TABLE_NAME
-WHERE TABLES.TABLE_NAME = 'first_table'
-AND TABLES.TABLE_SCHEMA = DATABASE();
+-- script that prints the following description of the table first_table from the database hbtn_0c_0
+SHOW CREATE TABLE first_table;
