cda4494e6c579ce80b37415a524b58153f394612
choukri33 <7646@holbertonstudents.com>
2024-06-21 14:06:02 +0000

    add taskk 5

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,4 @@
--- Créer la table unique_id avec les colonnes id et name
+-- Create the table unique_id with the columns id and name
 CREATE TABLE IF NOT EXISTS unique_id 
 (
     id INT DEFAULT 1 UNIQUE,
