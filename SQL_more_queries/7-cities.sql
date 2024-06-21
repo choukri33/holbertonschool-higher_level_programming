129ea019c52fb7e1abd924e163f10a4d0541be7c
choukri33 <7646@holbertonstudents.com>
2024-06-21 14:43:02 +0000

    add 8

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -10,3 +10,14 @@ CREATE TABLE IF NOT EXISTS cities
     name VARCHAR(256) NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(id)
 );
+-- Lister toutes les villes de Californie dans la base de données hbtn_0d_usa
+
+SELECT id, name
+FROM cities
+WHERE state_id = 
+	(
+	SELECT id
+	FROM states
+	WHERE name = 'California'
+	)
+ORDER BY id ASC;
