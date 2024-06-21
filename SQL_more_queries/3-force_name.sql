ed27dd0c02888d90f4608e534e157d5999ab9a28
choukri33 <7646@holbertonstudents.com>
2024-06-21 09:09:41 +0000

    add tables in hbtn

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,2 +1,6 @@
--- script that lists all the tables of a database
-SHOW TABLES;
+-- script that lists all the tables of a databases
+CREATE TABLE IF NOT EXISTS force_name 
+(
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
