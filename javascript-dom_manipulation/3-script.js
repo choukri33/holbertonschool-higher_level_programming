a97998f32d30a32d15714975966059dc3d737a70
choukri33 <7646@holbertonstudents.com>
2024-07-26 14:50:07 +0000

    add task fix

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,13 @@
+function toggleHeaderClass() {
+    const header = document.querySelector('header');
+    if (header.classList.contains('red')) {
+      header.classList.remove('red');
+      header.classList.add('green');
+    } else {
+      header.classList.remove('green');
+      header.classList.add('red');
+    }
+  }
+
+  document.getElementById('toggle_header').addEventListener('click', toggleHeaderClass);
+  
